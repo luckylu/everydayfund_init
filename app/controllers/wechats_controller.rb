c0232e27c7a7a2f8691801e29d5cc94a59245ed1
Michael Lu <646179989@qq.com>
2015-04-05 15:19:55 +0800

    test

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -1,2 +1,13 @@
 class WechatsController < ApplicationController
+	  wechat_responder
+
+  # 默认的文字信息responder
+  on :text do |request, content|
+    request.reply.text "echo: #{content}" #Just echo
+  end
+
+  # 当请求的文字信息内容为'help'时, 使用这个responder处理
+  on :text, with:"help" do |request, help|
+    request.reply.text "help content" #回复帮助信息
+  end
 end
